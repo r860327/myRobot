89096be116bdcec602ca7036b47d0464fc0c49b9
Chris Feng <chris.fhy@gmail.com>
2015-08-16 01:46:07 +0800

    Add cradlecamera.py module to control cradle head
    This devices is controled by pwm which is generated by a driver board
    connected to raspi via I2C
    
    Signed-off-by: Chris Feng <chris.fhy@gmail.com>

--- raspi_src/remoteControl.py
+++ raspi_src/remoteControl.py
@@ -4,8 +4,10 @@ from time import ctime
 from raspirobotboard import *
 import sys
 import RPi.GPIO as GPIO
+from cradlecamera import CameraServo
 
 rr = RaspiRobot()
+camera_servo = CameraServo()
 
 HOST = ''
 PORT = 20000
@@ -15,66 +17,10 @@ tcpSerSock = socket(AF_INET, SOCK_STREAM)
 tcpSerSock.bind(ADDR)
 tcpSerSock.listen(5)
 
-CAMERA_VERTICAL_CHANNEL = 23
-CAMERA_HORIZONTAL_CHANNEL = 18
-DEFAULT_VERTICAL_POS = 40
-DEFAULT_HORIZONTAL_POS = 82
-
-class Camera:
-	def __init__(self):
-		GPIO.setmode(GPIO.BCM)
-		GPIO.setup(CAMERA_VERTICAL_CHANNEL, GPIO.OUT)
-		GPIO.setup(CAMERA_HORIZONTAL_CHANNEL, GPIO.OUT)
-		self.vertical_p = GPIO.PWM(CAMERA_VERTICAL_CHANNEL, 50)
-		self.horizontal_p = GPIO.PWM(CAMERA_HORIZONTAL_CHANNEL, 50)
-
-		vertical_h_edge = 0.5 + DEFAULT_VERTICAL_POS * 2.0 / 180
-		horizontal_h_edge = 0.5 + DEFAULT_HORIZONTAL_POS * 2.0 / 180
-		self.current = cameraPosition(DEFAULT_VERTICAL_POS, DEFAULT_HORIZONTAL_POS)
-		self.vertical_p.start(100 * vertical_h_edge/(20.0 - vertical_h_edge))
-		self.horizontal_p.start(100 * horizontal_h_edge/(20.0 - horizontal_h_edge))
-		time.sleep(0.2)
-		self.vertical_p.ChangeDutyCycle(0)
-		self.horizontal_p.ChangeDutyCycle(0)
-
-	def pwmOut(self, channel, duty_cycle, frequence):
-		#GPIO.setmode(GPIO.BCM)
-		channel.ChangeDutyCycle(int(duty_cycle))
-#   	p.start(int(a))
-		print('channel: ' + str(channel))
-		print('duty_cycle: ' + str(duty_cycle))
-		print('frequence: ' + str(frequence))
-
-		time.sleep(0.1)
-		channel.ChangeDutyCycle(0)
-#		GPIO.cleanup()
-
-	def setCameraPostion(self, command):
-		if command == 'udc' and self.current.vertical > 0:
-			self.current.vertical = max(0, self.current.vertical - 15)
-			high_level = 0.5 + self.current.vertical * 2.0 / 180
-			self.pwmOut(self.vertical_p, 100 * high_level/(20.0 - high_level), 50)
-		elif command == 'ddc' and self.current.vertical < 120:
-			self.current.vertical = min(120, self.current.vertical + 15)
-			high_level = 0.5 + self.current.vertical * 2.0 / 180
-			self.pwmOut(self.vertical_p, 100 * high_level/(20.0 - high_level), 50)
-		elif command == 'rdc' and self.current.horizontal > 20:
-			self.current.horizontal = max(10, self.current.horizontal - 15)
-			high_level = 0.5 + self.current.horizontal * 2.0 / 180
-			self.pwmOut(self.horizontal_p, 100 * high_level/(20.0 - high_level), 50)
-		elif command == 'ldc' and self.current.horizontal < 160:
-			self.current.horizontal = min(160, self.current.horizontal + 15)
-			high_level = 0.5 + self.current.horizontal * 2.0 / 180
-			self.pwmOut(self.horizontal_p, 100 * high_level/(20.0 - high_level), 50)
-		else:
-			print("unknow command : " + str(command) + " or need no nothing!\n")
-
-class cameraPosition:
-	def __init__(self, vertical, horizontal):
-		self.vertical = vertical
-		self.horizontal = horizontal
-
-cam = Camera()
+CAM_HORIZONTAL_DIR = 0
+CAM_VERTICAL_DIR = 1
+CAM_ANGLE_DECREASE = 0
+CAM_ANGLE_INCREASE = 1
 
 while True:
 	print 'waiting for connection...'
@@ -94,8 +40,14 @@ while True:
 			rr.reverse(0.075)
 		elif data == 'rd':
 			rr.right(0.075)
-		elif data == 'udc' or data == 'ddc' or data == 'ldc' or data == 'rdc':
-			cam.setCameraPostion(data)
+		elif data == 'udc':
+			camera_servo.update_pos(CAM_ANGLE_DECREASE, CAM_VERTICAL_DIR, 10)
+		elif data == 'ddc':
+			camera_servo.update_pos(CAM_ANGLE_INCREASE, CAM_VERTICAL_DIR, 10)
+		elif data == 'ldc':
+			camera_servo.update_pos(CAM_ANGLE_INCREASE, CAM_HORIZONTAL_DIR, 10)
+		elif data == 'rdc':
+			camera_servo.update_pos(CAM_ANGLE_DECREASE, CAM_HORIZONTAL_DIR, 10)
 		else:
 			print 'unknow command'
 	tcpClientSock.close()
